f8d8f32b16f17380f473ddf992ac0afa15344b17
Shubham Kumar <shubham620409@gmail.com>
2026-08-06 20:54:40 +0530

    Create SectionTitle.jsx

---
+++ client/src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import "./SectionTitle.css";
+
+function SectionTitle({ title, subtitle }) {
+  return (
+    <div className="section-title-wrapper">
+      <h2 className="section-title">{title}</h2>
+
+      {subtitle && <p className="section-subtitle">{subtitle}</p>}
+    </div>
+  );
+}
+
+export default SectionTitle;
