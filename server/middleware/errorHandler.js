157442c8f8bbbc3f98474af30be624940cde9760
Shubham Kumar <shubham620409@gmail.com>
2026-08-07 11:10:27 +0530

    Create errorHandler.js

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+    let statusCode =
+        res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+    });
+};
+
+module.exports = errorHandler;
